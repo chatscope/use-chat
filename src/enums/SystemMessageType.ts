bae1b2f58ea25fc06cf25f5bb62ca6232f3fbcde
Super Snager <supersnager@gmail.com>
2023-03-09 14:02:49 +0100

    fix: wrong SystemMessageType enum values

--- src/enums/SystemMessageType.ts
+++ src/enums/SystemMessageType.ts
@@ -7,8 +7,8 @@ export enum SystemMessageType {
   Content = 0, // Client should display content from the content property
   UserJoined = 1, // Content property contains the name of the user who joined the conversation
   UserLeft = 2, // Content property contains the name of the user who left the conversation
-  AdvisorJoined = 1, // Content property contains the name of the advisor who joined the conversation
-  AdvisorLeft = 2, // Content property contains the name of the advisor who left the conversation
-  CustomerJoined = 3, // Content property contains the name of the customer who joined the conversation
-  CustomerLeft = 4, // Content property contains the name of the customer who left the conversation
+  AdvisorJoined = 3, // Content property contains the name of the advisor who joined the conversation
+  AdvisorLeft = 4, // Content property contains the name of the advisor who left the conversation
+  CustomerJoined = 5, // Content property contains the name of the customer who joined the conversation
+  CustomerLeft = 6, // Content property contains the name of the customer who left the conversation
 }
