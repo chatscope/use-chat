10d5843d7640dbf23622868216f0653143c64682
odelolajosh <oladapoodelola@gmail.com>
2023-07-18 19:54:42 +0100

    feat: add `state` field to ConnectionStateChangedEvent class

--- src/events/ConnectionStateChangedEvent.ts
+++ src/events/ConnectionStateChangedEvent.ts
@@ -1,7 +1,9 @@
-import { ChatEventType } from "../enums";
+import { ChatEventType, ConnectionState } from "../enums";
 import { ChatEvent } from "./ChatEvent";
 
 export class ConnectionStateChangedEvent
   implements ChatEvent<ChatEventType.ConnectionStateChanged> {
   readonly type = ChatEventType.ConnectionStateChanged;
+
+  constructor(readonly state: ConnectionState) {}
 }
